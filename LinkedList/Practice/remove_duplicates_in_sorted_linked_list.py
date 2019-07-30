ee2e62abd92d67ed6dd78dd5ab221716115840c3
AbKumarSingh <Abhishek.kumar.singh@microfocus.com>
2019-07-31 00:35:34 +0530

    Remove duplicates from sorted linked list

---
+++ LinkedList/Practice/remove_duplicates_in_sorted_linked_list.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+# Linked List Class
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    # creates a new node with given value and appends it at the end of the linked list
+
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            return
+        curr_node = self.head
+        while curr_node.next is not None:
+            curr_node = curr_node.next
+        curr_node.next = new_node
+    # prints the elements of linked list starting with head
+
+    def printList(self):
+        if self.head is None:
+            print(' ')
+            return
+        curr_node = self.head
+        while curr_node:
+            print(curr_node.data, end=" ")
+            curr_node = curr_node.next
+        print(' ')
+
+
+def removeDuplicates(head):
+    next_= None
+    while head and head.next:
+        if head.data == head.next.data:
+            curr = head
+            next_ = curr.next
+        head = head.next
+    head = next_
+    return head
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for cases in range(t):
+        n = int(input())
+        a = LinkedList()  # create a new linked list 'a'.
+        nodes = list(map(int, input().strip().split()))
+        for x in nodes:
+            a.append(x)
+        a.head = removeDuplicates(a.head)
+        a.printList()
