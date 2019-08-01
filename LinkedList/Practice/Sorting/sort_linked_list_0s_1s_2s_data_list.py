143cb3d7c18014c080df848a22fede37b9bf9ef4
AbKumarSingh <Abhishek.kumar.singh@microfocus.com>
2019-08-02 00:23:22 +0530

    sort 0s 1s 2s nodes linked list changing data

---
+++ LinkedList/Practice/Sorting/sort_linked_list_0s_1s_2s_data_list.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+# Linked List Class
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    # creates a new node with given value and appends it at the end of the linked list
+
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            return
+        curr_node = self.head
+        while curr_node.next is not None:
+            curr_node = curr_node.next
+        curr_node.next = new_node
+# prints the elements of linked list starting with head
+
+
+def printList(head):
+    if head is None:
+        print(' ')
+        return
+    curr_node = head
+    while curr_node:
+        print(curr_node.data, end=" ")
+        curr_node = curr_node.next
+    print(' ')
+
+
+def segregate(head):
+    p1 = head
+    my_list = []
+    while p1:
+        my_list.append(p1.data)
+        p1 = p1.next
+    my_list.sort()
+    p2 = head
+    i = 0
+    while p2:
+        p2.data = my_list[i]
+        p2 = p2.next
+        i+=1
+    return head
+
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for cases in range(t):
+        n = int(input())
+        a = LinkedList()  # create a new linked list 'a'.
+        nodes_a = list(map(int, input().strip().split()))
+        for x in nodes_a:
+            a.append(x)  # add to the end of the list
+        a.head = segregate(a.head)
+        printList(a.head)
