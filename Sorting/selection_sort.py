f169659e328856a5e4e6f89f2f0526ef79a00b36
AbKumarSingh <Abhishek.kumar.singh@microfocus.com>
2019-08-14 00:52:23 +0530

    modification in selection Sort

--- Sorting/selection_sort.py
+++ Sorting/selection_sort.py
@@ -1,10 +1,8 @@
-a = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+a = [119, 160, 390, 947, 604, 251 ]
 for i in range(len(a) - 1):
     min_index = i
     for j in range(i + 1, len(a)):
         if a[j] < a[i]:
             min_index = j
-
-    if min_index != i:
         a[i], a[min_index] = a[min_index], a[i]
 print(a)
