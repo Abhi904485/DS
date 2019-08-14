4b3122d94e11ed1921e93b1f985629a829ca34cc
AbKumarSingh <Abhishek.kumar.singh@microfocus.com>
2019-08-15 01:15:15 +0530

    max and second max in unsorted array

---
+++ Array/Practice/max_and_second_max_element_in_unsorted_array.py
@@ -0,0 +1,22 @@
+def find_max_and_second_max_element(arr, n):
+    first_max = arr[0]
+    second_max = None
+    for i in range(1, n):
+        if arr[i] > first_max:
+            second_max = first_max
+            first_max = arr[i]
+        else:
+            if not second_max and arr[i] < first_max:
+                second_max = arr[i]
+            if arr[i] < first_max and arr[i] > second_max:
+                second_max = arr[i]
+
+    return second_max if second_max else -1
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().split()))
+        print(find_max_and_second_max_element(arr, n))
